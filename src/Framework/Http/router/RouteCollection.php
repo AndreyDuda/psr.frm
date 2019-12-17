f4256a15f34004c945954ea95cffbc44fdb173d0
AndreyDuda <AndreyDuda88@gmail.com>
2019-12-17 23:26:14 +0200

    create sheme route part 1

---
+++ src/Framework/Http/router/RouteCollection.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Framework\Http\router;
+
+class RouteCollection
+{
+    private $routes = [];
+
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+}
